125eedfad4d4ed965dab62ef749838acfc1ca385
anissa-111 <zacharia@my.erau.edu>
2016-10-14 14:09:23 -0400

    Register skeleton

---
+++ Register.java
@@ -0,0 +1,43 @@
+/********************************************************************************
+File Name: Register.java
+
+Attributes List:
+	
+	
+Methods List:
+	
+	
+********************************************************************************/
+
+import java.util.ArrayList;
+import java.io.*;
+import java.math.*;
+import java.util.Arryas;
+
+public class Register {
+
+	
+	//Array lists of Employees and Items
+	ArrayList<Employee> employees = new ArrayList<Employee>();
+	//ArrayList<Item> items = new ArrayList<Item>(); class not created yet
+	
+	
+	//Party Array lists
+	ArrayList<Party> waitingParties = new ArrayList<Party>();
+	ArrayList<Party> activeParties = new ArrayList<Party>();
+	
+	
+	//Payroll (not yet)
+	
+	
+	BigDouble currentCash;
+	
+	Register() {
+		//populate employees and items
+		
+		
+		
+	}
+		
+	
+}
