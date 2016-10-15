6620fd0b080279113b44f0840d4ddc5c3d2ad26e
12sp21 <github@spencermowrey.com>
2016-10-15 18:30:42 -0400

    Update HostGUI.java

--- HostGUI.java
+++ HostGUI.java
@@ -426,6 +426,40 @@ public class HostGUI extends Application{
 		POSButton clockIn = new POSButton(50,150,"Clock In");
 		POSButton clockOut = new POSButton(50,150,"Clock Out");
 		
+		//Disables the clock out function while you haven't clocked in yet, and saves clock in/clock out times for the file
+		clockOut.setDisable(true);
+		clockIn.setOnAction(e -> {
+			SimpleDateFormat ddMMyyyyhhmmss = new SimpleDateFormat("dd/MM/yyyy_hh:mm:ss");
+			String clockInDate;
+			Date createdTime = new Date();
+			clockInDate = new String(ddMMyyyyhhmmss.format(createdTime));
+			System.out.println(clockInDate);
+			 try {
+				BufferedWriter inverseBucket = new BufferedWriter(new FileWriter("clockinclockoutdata.txt",true));
+				inverseBucket.write(String.format("%n%s				",clockInDate));
+				inverseBucket.close();
+				clockIn.setDisable(true);
+				clockOut.setDisable(false);
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+		});
+		clockOut.setOnAction(e -> {
+			SimpleDateFormat ddMMyyyyhhmmss = new SimpleDateFormat("dd/MM/yyyy_hh:mm:ss");
+			String clockOutDate;
+			Date createdTime = new Date();
+			clockOutDate = new String(ddMMyyyyhhmmss.format(createdTime));
+			System.out.println(clockOutDate);
+			 try {
+				BufferedWriter inverseBucket = new BufferedWriter(new FileWriter("clockinclockoutdata.txt",true));
+				inverseBucket.write(String.format("%s",clockOutDate));
+				inverseBucket.close();
+				clockIn.setDisable(false);
+				clockOut.setDisable(true);
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+		});
 		
 		quitPane.getChildren().addAll(quitText,quitYes,quitNo);
 		optionsPane.getChildren().addAll(clockIn,clockOut,optionsQuit);
