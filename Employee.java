01ee4fe7f6b0cb00177483f3b030abdb88bc1b68
anissa-111 <zacharia@my.erau.edu>
2016-10-16 19:20:21 -0400

    Skeleton for employee class and IO file

---
+++ Employee.java
@@ -0,0 +1,31 @@
+public class Employee{
+	String name;
+	double price;
+	int ID;
+	double wage;
+	int phone;
+	String address;
+	String email;
+	int SSN;
+	int clearance;
+	double shiftHours;
+	
+	//constructor
+	Item(String name int ID, double wage, int phone, String address, String email, int SSN, int clearance) {
+		this.name = name;
+		this.ID = ID;
+		this.wage = wage;
+		this.phone = phone;
+		this.address = address;
+		this.email = email;
+		this.SSN = SSN;
+		this.clearance = clearance;
+	}
+
+	
+	public String getName(){
+		return this.name;
+	}
+	
+
+}
