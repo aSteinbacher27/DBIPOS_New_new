c1f776b4958c0d263c34ab546530da63a3b51a75
bjaury <brian.jaury@gmail.com>
2016-10-19 00:19:57 -0400

    Fixed HostGUI
    Accidentally Enabled code that was removed prior regarding clocking in
    and out.

--- HostGUI.java
+++ HostGUI.java
@@ -526,52 +526,53 @@ public class HostGUI extends Application{
 		POSButton clockOut = new POSButton(50,150,"Clock Out");
 		clockOut.setStyle("-fx-font-size: 16px");
 		
-		//Disables the clock out function while you haven't clocked in yet, and saves clock in/clock out times for the file
-		clockOut.setDisable(true);
-		clockIn.setOnAction(e -> {
-			SimpleDateFormat ddMMyyyyhhmmss = new SimpleDateFormat("dd/MM/yyyy_hh:mm:ss");
-			String clockInDate;
-			Date createdTime = new Date();
-			clockInDate = new String(ddMMyyyyhhmmss.format(createdTime));
-			System.out.println(clockInDate);
-			try {
-				BufferedWriter inverseBucket = new BufferedWriter(new FileWriter("clockinclockoutdata.txt",true));
-				inverseBucket.write(String.format("%n%s				",clockInDate));
-				inverseBucket.close();
-				clockIn.setDisable(true);
-				clockOut.setDisable(false);
-			} catch (Exception e1) {
-				e1.printStackTrace();
-			}
-		});
-		clockOut.setOnAction(e -> {
-			SimpleDateFormat ddMMyyyyhhmmss = new SimpleDateFormat("dd/MM/yyyy_hh:mm:ss");
-			String clockOutDate;
-			Date createdTime = new Date();
-			clockOutDate = new String(ddMMyyyyhhmmss.format(createdTime));
-			System.out.println(clockOutDate);
-			try {
-				BufferedWriter inverseBucket = new BufferedWriter(new FileWriter("clockinclockoutdata.txt",true));
-				inverseBucket.write(String.format("%s",clockOutDate));
-				inverseBucket.close();
-				clockIn.setDisable(false);
-				clockOut.setDisable(true);
-				clockIn.setOnAction(e -> {
-					SimpleDateFormat ddMMyyyyhhmmss = new SimpleDateFormat("dd/MM/yyyy_hh:mm:ss");
-					String clockInDate;
-					Date createdTime = new Date();
-					clockInDate = new String(ddMMyyyyhhmmss.format(createdTime));
-					System.out.println(clockInDate);
-					 try {
-						BufferedWriter inverseBucket = new BufferedWriter(new FileWriter("clockinclockoutdata.txt",true));
-						inverseBucket.write(String.format("%n%s				",clockInDate));
-						inverseBucket.close();
-						clockIn.setDisable(true);
-						clockOut.setDisable(false);
-					} catch (Exception e1) {
-						e1.printStackTrace();
-					}
-				});
+		// //Disables the clock out function while you haven't clocked in yet, and saves clock in/clock out times for the file
+		// clockOut.setDisable(true);
+		// clockIn.setOnAction(e -> {
+		// 	SimpleDateFormat ddMMyyyyhhmmss = new SimpleDateFormat("dd/MM/yyyy_hh:mm:ss");
+		// 	String clockInDate;
+		// 	Date createdTime = new Date();
+		// 	clockInDate = new String(ddMMyyyyhhmmss.format(createdTime));
+		// 	System.out.println(clockInDate);
+		// 	try {
+		// 		BufferedWriter inverseBucket = new BufferedWriter(new FileWriter("clockinclockoutdata.txt",true));
+		// 		inverseBucket.write(String.format("%n%s				",clockInDate));
+		// 		inverseBucket.close();
+		// 		clockIn.setDisable(true);
+		// 		clockOut.setDisable(false);
+		// 	} catch (Exception e1) {
+		// 		e1.printStackTrace();
+		// 	}
+		// });
+		// clockOut.setOnAction(e -> {
+		// 	SimpleDateFormat ddMMyyyyhhmmss = new SimpleDateFormat("dd/MM/yyyy_hh:mm:ss");
+		// 	String clockOutDate;
+		// 	Date createdTime = new Date();
+		// 	clockOutDate = new String(ddMMyyyyhhmmss.format(createdTime));
+		// 	System.out.println(clockOutDate);
+		// 	try {
+		// 		BufferedWriter inverseBucket = new BufferedWriter(new FileWriter("clockinclockoutdata.txt",true));
+		// 		inverseBucket.write(String.format("%s",clockOutDate));
+		// 		inverseBucket.close();
+		// 		clockIn.setDisable(false);
+		// 		clockOut.setDisable(true);
+		// 		clockIn.setOnAction(e -> {
+		// 			SimpleDateFormat ddMMyyyyhhmmss = new SimpleDateFormat("dd/MM/yyyy_hh:mm:ss");
+		// 			String clockInDate;
+		// 			Date createdTime = new Date();
+		// 			clockInDate = new String(ddMMyyyyhhmmss.format(createdTime));
+		// 			System.out.println(clockInDate);
+		// 			 try {
+		// 				BufferedWriter inverseBucket = new BufferedWriter(new FileWriter("clockinclockoutdata.txt",true));
+		// 				inverseBucket.write(String.format("%n%s				",clockInDate));
+		// 				inverseBucket.close();
+		// 				clockIn.setDisable(true);
+		// 				clockOut.setDisable(false);
+		// 			} catch (Exception e1) {
+		// 				e1.printStackTrace();
+		// 			}
+		// 		});
+		// 	}
 				clockOut.setOnAction(e -> {
 					SimpleDateFormat ddMMyyyyhhmmss = new SimpleDateFormat("dd/MM/yyyy_hh:mm:ss");
 					String clockOutDate;
