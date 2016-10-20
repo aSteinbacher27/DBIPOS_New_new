b27b0baff02a6c83c4dd4e71dfcffc06e4fedfa4
12sp21 <github@spencermowrey.com>
2016-10-20 14:24:00 -0400

    Update POSButton.java

--- POSButton.java
+++ POSButton.java
@@ -1,10 +1,18 @@
 import javafx.scene.control.Button;
-
+/** POSButton class takes a height, width, and string parameter and generates a button using these parameters
+ *
+ * @author Alex Steinbacher
+ * @version 1.0
+ */
 public class POSButton extends Button{
 	double prefH;
 	double prefW;
 	String txt;
-	
+	/**
+	 * @param prefH : The desired height of the button
+	 * @param prefW : The desired width of the button
+	 * @param txt : The desired text on the button
+	 */
 	public POSButton(double prefH, double prefW, String txt){
 		this.prefH = prefH;
 		this.prefW = prefW;
