2ed4fe9f3f84d5eeaf087d38ff2288a94d930b82
wilsok21 <wilsok21@my.erau.edu>
2016-10-19 20:56:37 -0400

    Items sorted by category. Add/Remove buttons work!

--- ServerGUI.java
+++ ServerGUI.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Vector;
-
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
