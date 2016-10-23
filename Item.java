455d2fcc25e434e8d3391c687a3c5ad5f50e2ea0
anissa-111 <zacharia@my.erau.edu>
2016-10-23 17:45:23 -0400

    completed javadocs

--- Item.java
+++ Item.java
@@ -7,21 +7,50 @@
  */
 public class Item {
 	
+	/**
+	 * name of item
+	 */
 	String name;
+	
+	/**
+	 * price of object as a double
+	 */
 	double price;
+	
+	/**
+	 * type classification of item (menu, gift, drink, dessert) as a string
+	 */
 	String type;
 	
-	//constructor
+
+	/**
+	 * Item constructor
+	 * 
+	 * @param name
+	 * @param type
+	 * @param price
+	 */
 	Item(String name, String type, double price) {
 		this.name = name;
 		this.type = type;
 		this.price = price;
 	}
 	
+	
+	/**
+	 * Getter for price
+	 * 
+	 * @return price
+	 */
 	public double getPrice(){
 		return this.price;
 	}
 	
+	/**
+	 * Getter for name
+	 * 
+	 * @return name
+	 */
 	public String getName(){
 		return this.name;
 	}
