101b4a4eeef0604e9ea18d74f8866014b987246d
wilsok21 <wilsok21@my.erau.edu>
2016-10-26 16:50:35 -0400

    git updates

---
+++ DBIPOS/src/app/Employee.java
@@ -0,0 +1,44 @@
+package app;
+/**
+ * @author
+ * 
+ * @version 1.0
+ * 
+ *This class generates an employee with all attributes needed
+ */
+public class Employee{
+	String name;
+	double price;
+	int ID;
+	double wage;
+	int phone;
+	String address;
+	String email;
+	int SSN;
+	int clearance;
+	double shiftHours;
+	
+	//constructor
+	Employee(String name, int ID, double wage, int phone, String address, String email, int SSN, int clearance) {
+		this.name = name;
+		this.ID = ID;
+		this.wage = wage;
+		this.phone = phone;
+		this.address = address;
+		this.email = email;
+		this.SSN = SSN;
+		this.clearance = clearance;
+	}
+
+	
+	public String getName(){
+		return this.name;
+	}
+
+	public int getID(){
+	
+		return ID;
+	}
+	
+
+}
